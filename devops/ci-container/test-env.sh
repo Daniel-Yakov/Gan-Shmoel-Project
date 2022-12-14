428e7fa4448cd64ea4dc877d90f54b388579410e
Daniel Yakov <daniel577000@gmail.com>
2022-12-14 17:53:31 +0200

    modified test-env.sh

--- devops/ci-container/test-env.sh
+++ devops/ci-container/test-env.sh
@@ -2,17 +2,24 @@
 
 # pull the new version to be tested
 git init
-git remote add origin https://github.com/Daniel-Yakov/Gan-Shmoel-Project.git
-git pull origin weight
-git pull origin billing
+git remote add origin https://github.com/Daniel-Yakov/test-env.git
+git pull origin main
 
-# create the container with docker compose
-   
+# create the containers with docker compose
+
+export WEIGHT_APP_PORT=8081
+export BILLING_APP_PORT=8082
+
+# weight app
+docker-compose -f ./weight/docker-compose.yaml build --no-cache
+docker-compose -f ./weight/docker-compose.yaml up -d
+
+# billing app
+docker-compose -f ./billing/docker-compose.yml build --no-cache
+docker-compose -f ./billing/docker-compose.yml up -d
+
 
 # run tests
 
 
-# Failure - send mails
 
-
-# Success send maills and deploy new image
