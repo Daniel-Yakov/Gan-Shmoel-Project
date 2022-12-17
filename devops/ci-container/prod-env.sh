fa5d14ec18d17616a6c4d85692c78c14918dcae2
Daniel Yakov <daniel577000@gmail.com>
2022-12-17 22:31:54 +0200

    ci version 18

--- devops/ci-container/prod-env.sh
+++ devops/ci-container/prod-env.sh
@@ -4,9 +4,6 @@ export WEIGHT_APP_PORT=8089
 export BILLING_APP_PORT=8090
 export NETWORK=prod-env
 
-# cp ./weight/docker-compose.yaml ./weight/docker-compose-w-prod.yml
-# cp ./billing/docker-compose.yml ./billing/docker-compose-b-prod.yml
-
 # remove prevoius prod container
 docker-compose -f ./weight/docker-compose-w-test.yml -p prod down
 docker-compose -f ./billing/docker-compose-b-test.yml -p prod down
@@ -15,5 +12,4 @@ docker-compose -f ./billing/docker-compose-b-test.yml -p prod down
 docker-compose -f ./weight/docker-compose-w-test.yml -p prod up -d --build
 docker-compose -f ./billing/docker-compose-b-test.yml -p prod up -d --build
 
-# rm ./weight/docker-compose-w-prod.yml ./billing/docker-compose-b-prod.yml
 rm -rf ./weight/docker-compose-w-test.yml ./billing/docker-compose-b-test.yml
