e912025c9a6c228adbe3ff90655351e08280263d
shoval <shoval12355@gmail.com>
2022-12-17 19:11:46 +0200

    test with devops1

--- weight/test.sh
+++ weight/test.sh
@@ -2,11 +2,11 @@
 echo "INITIATING TEST as weight team"
 sleep 1.5
 echo "STARTING HEALTH TEST as weight team"
-output=$(curl "http://ec2-18-168-15-244.eu-west-2.compute.amazonaws.com/:$WEIGHT_APP_PORT/health)
+output=$(curl "ubuntu@ec2-3-10-71-229.eu-west-2.compute.amazonaws.com:${WEIGHT_APP_PORT}/health")
 
 if [ "$output" = '"APP ON AIR"' ]; then
     echo "Passed the health assesment"
-    echo "Passed the health assesment"
+    
 
 elif [ "$output" = '"OK 200 & BAD Connection"' ]
 		echo "failed health to database assesment"
